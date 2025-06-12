84e5b6ddd8509b6ee63eadbade054667071d1c09
Siddharth Shehria <83392744+sidshehria@users.noreply.github.com>
2025-06-12 08:45:30 +0530

    Update environment.ts

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -1,6 +1,6 @@
 
 export const environment = {
   production: false,
-  GEMINI_API_KEY: 'AIzaSyC8SQ_SsM4erpOj_e4mdJ-SJJJIJMbFEo0',
-  GEMINI_API_URL: 'https://generativelanguage.googleapis.com/v1beta/models/gemini-pro:generateContent'
+  GEMINI_API_KEY: 'enter your key here',
+  GEMINI_API_URL: 'enter your URL here'
 };
